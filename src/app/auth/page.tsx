147ce2d5578502e0ecf1de42b518c7162aaedde0
Giancarlo Andretta <gandretta@allentowninc.com>
2025-03-01 09:42:17 -0500

    Initial commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,17 @@
+const Auth = () => {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.png')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:opacity-50">
+
+                <nav className="">
+
+                </nav>
+
+
+
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+
+
+const myPage = () => {
+    return (
+        <h1 className="text-red-500">Hello New Page</h1>
+    );
+}
+
+export default myPage;
